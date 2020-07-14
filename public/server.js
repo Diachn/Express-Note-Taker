217ff65527d420fc6469a4134e914c5b60186892
Diachn <Diiianachen@gmail.com>
2020-07-13 21:36:34 -0700

    Removed console logs

--- public/server.js
+++ public/server.js
@@ -10,7 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "notes.html"));
+    res.sendFile(path.join(__dirname, "/notes.html"));
 });
 
 app.get("/api/notes", function(req, res) {
@@ -22,8 +22,8 @@ app.get("/api/notes/:id", function(req, res) {
     res.json(savedNotes[Number(req.params.id)]);
 });
 
-app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "index.html"));
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "/index.html"));
 });
 
 app.post("/api/notes", function(req, res) {
